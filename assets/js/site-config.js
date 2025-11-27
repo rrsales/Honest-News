9ab786e9e73a9619b2be4189857f7fe8c6f066bb
rrsales <josephbskinner1@gmail.com>
2025-11-26 20:30:57 -0500

    Update site-config.js
    Chatgpt

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -1,8 +1,17 @@
-window.SITE_CONFIG = {
-  menu: {},
-  hero: {},
-  pages: [],
-  version: "1.0"
+// site-config.js
+const SITE_CONFIG = {
+  name: "Honest News Network",
+  storageKey: "honestnews_siteData_v1", // must match admin.js
+  heroDefaults: {
+    height: "60vh",
+    slides: [
+      "assets/images/hero/hero1.jpg",
+      "assets/images/hero/hero2.jpg",
+      "assets/images/hero/hero3.jpg"
+    ]
+  },
+  debug: false
 };
 
+if (SITE_CONFIG.debug) console.log("Site config loaded", SITE_CONFIG);
 
