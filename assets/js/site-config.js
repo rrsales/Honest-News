0350c0e0cd03bf7d2455e99d4a2ca68f305c6d01
rrsales <josephbskinner1@gmail.com>
2025-11-26 20:15:19 -0500

    Update site-config.js

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -1,10 +1,8 @@
-// site-config.js
-const SITE = {
-  name: "Honest News Network",
-  version: "1.0",
-  apiBase: "", // for future use (blog, store, etc.)
-  debug: false
+window.SITE_CONFIG = {
+  menu: {},
+  hero: {},
+  pages: [],
+  version: "1.0"
 };
 
-console.log("Site config loaded:", SITE);
 
